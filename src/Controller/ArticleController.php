3b32fb5c0b0bc66378ea13c1350c62a2d0311fa0
william <william.del.rosario@iceshop.nl>
2019-09-15 18:05:37 +0200

    Debug container cache config -- php bin/console debug:autowiring                                 php bin/console debug:container --show-private                                 php bin/console config:dump framework                                 php bin/console debug:config framework // current config

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -40,8 +40,8 @@ EOF;
             $cache->save($item);
         }
 
+        dd($cache);
         $articleContent = $item->get();
-        dd($markdown);
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
