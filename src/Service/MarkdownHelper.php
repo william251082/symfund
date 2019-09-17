fc9cde60fd8809fd407d06472be085d6227d89e4
william <william.del.rosario@iceshop.nl>
2019-09-17 21:54:11 +0200

    Service config bind -- accessing services that can not be autowired

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
@@ -11,14 +12,21 @@ class MarkdownHelper
 
     private $markdown;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+    private $logger;
+
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
+        $this->logger = $markdownLogger;
     }
 
     public function parse(string $source): string
     {
+        if (stripos($source, 'bacon') !== false) {
+            $this->logger->info('They are talking bacon again!');
+        }
+
         $item = $this->cache->getItem('markdown_'.md5($source));
         if ($item->isHit()) {
             $item->set($this->markdown->transform($source));
