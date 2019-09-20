2bba6f21ed70bc2d8ff22f6f242da1896da27285
william <william.del.rosario@iceshop.nl>
2019-09-20 22:27:18 +0200

    Article stats command

--- src/Command/ArticleStatsCommand.php
+++ src/Command/ArticleStatsCommand.php
@@ -16,25 +16,31 @@ class ArticleStatsCommand extends Command
     protected function configure()
     {
         $this
-            ->setDescription('Add a short description for your command')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->setDescription('Returns some article stats')
+            ->addArgument('slug', InputArgument::REQUIRED, 'The article\'s slug')
+            ->addOption('format', null, InputOption::VALUE_OPTIONAL, 'The output format, text')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $slug = $input->getArgument('slug');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        $data = [
+            'slug' => $slug,
+            'hearts' => rand(10, 100),
+        ];
+
+        switch ($input->getOption('format')) {
+            case 'text':
+                $io->listing($data);
+                break;
+            case 'json':
+                $io->write(\GuzzleHttp\json_encode($data));
+                break;
+            default:
+                throw new \Exception('Format not allowed');
         }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
     }
 }
