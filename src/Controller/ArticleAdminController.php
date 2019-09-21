fb641a0cafcec29808d4429a45be18dded6b0d80
william <william.del.rosario@iceshop.nl>
2019-09-21 13:15:31 +0200

    New filed for article entity

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -28,6 +28,10 @@ EOF
             $article->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
         }
 
+        $article->setAuthor('Mike Ferengi')
+            ->setHeartCount(rand(5, 100))
+            ->setImageFilename('asteroid.jpeg');
+
         $manager->persist($article);
         $manager->flush();
 
