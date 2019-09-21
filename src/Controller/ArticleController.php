32ac1b671bd221d91e8def227f3e481ac8558d28
william <william.del.rosario@iceshop.nl>
2019-09-21 13:34:40 +0200

    Updating entity

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -56,12 +56,13 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    public function toggleArticleHeart(Article $article, LoggerInterface $logger, EntityManagerInterface $manager)
     {
-        // TODO - actually heart/unheart the article!
+        $article->incrementHeartCount();
+        $manager->flush();
 
         $logger->info('Article is being hearted!');
 
-        return new JsonResponse(['hearts' => rand(5, 100)]);
+        return new JsonResponse(['hearts' => $article->getHeartCount()]);
     }
 }
